d1393fc8b16bea0696df4e335def0d2dcfac04d0
miguelhdez <miketronichp@outlook.com>
2018-04-29 22:03:43 -0400

    Erase

--- app/src/main/java/com/example/aptivist/weatherapp/data/RepositoryContract.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.watherapp.data;
-
-import com.example.android.watherapp.data.entities.WeatherObj;
-
-public interface RepositoryContract {
-    void onSucces(WeatherObj weatherObj);
-    void onError();
-}
